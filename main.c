182c80768a2a4d110d445a06e2eeb499543ea1bb
yuri-deutsh <66663102+yuri-deutsh@users.noreply.github.com>
2024-11-02 19:02:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
+/* This source code copyrighted by Lazy Foo' Productions (2004-2022)
 and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
